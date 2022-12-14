3ac1d7ef559d40f84ce45298c0867c56426ccac2
piotrkaliszuk <108789004+piotrkaliszuk@users.noreply.github.com>
2022-12-14 20:36:54 +0100

    Add files via upload

---
+++ LSTMimplementacja.py
@@ -0,0 +1,42 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.models import Sequential
+from labels import zbiorX, Y
+from sklearn.model_selection import train_test_split
+from keras.layers import LSTM, Dense
+print(np.array(Y).shape)
+print(np.array(zbiorX).shape)
+
+X_train, X_test, y_train, y_test = train_test_split(zbiorX,Y, train_size=0.9)
+
+"""print(np.array(X_train).shape)
+print(np.array(y_train).shape)
+print(np.array(X_test).shape)
+print(np.array(y_test).shape)
+"""
+
+n_epochs = 10
+batch_size = 5
+
+model = Sequential()
+model.add(LSTM(units = 3, input_shape = (120, 3)))
+model.add(Dense(units = 3, activation='relu'))
+model.add(Dense(1, activation = 'softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.summary()
+history = model.fit(X_train, y_train, epochs = n_epochs, validation_split = 0.30, batch_size = batch_size, verbose = 1)
+
+plt.plot(np.array(history.history['loss']), "r--", label = "Train loss")
+plt.plot(np.array(history.history['accuracy']), "g--", label = "Train accuracy")
+plt.plot(np.array(history.history['val_loss']), "r-", label = "Validation loss")
+plt.plot(np.array(history.history['val_accuracy']), "g-", label = "Validation accuracy")
+plt.title("Training session's progress over iterations")
+plt.legend(loc='lower left')
+plt.ylabel('Training Progress (Loss/Accuracy)')
+plt.xlabel('Training Epoch')
+plt.ylim(0)
+plt.show()
+
+loss, accuracy = model.evaluate(X_test, y_test, batch_size = batch_size, verbose = 1)
+print("Test Accuracy :", accuracy)
+print("Test Loss :", loss)
