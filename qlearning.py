faecb62a2ce03b61a6a7b912fce05c9d8b0bd2eb
piotrkaliszuk <kaliszuk_piotr@wp.pl>
2023-02-22 22:26:47 +0100

    QLEARNING ORAZ GENEROWANIE SRODOWISKA W POSTACI TABELI

--- qlearning.py
+++ qlearning.py
@@ -1,19 +1,19 @@
 import numpy as np
 srodowisko_wiersze = 6
 srodowisko_kolumny = 6
-wartosciQ = np.zeros((srodowisko_wiersze, srodowisko_kolumny, 4))
+wartosciQ = np.zeros((srodowisko_wiersze, srodowisko_kolumny, 4)) #Q
 #print(wartosciQ)
-kierunki = ['up', 'right', 'down', 'left']
+kierunki = ['up', 'down', 'right', 'left']
 
-nagrody = np.full((srodowisko_wiersze, srodowisko_kolumny), -100)
-nagrody[0, 3] = 100
+nagrody = np.full((srodowisko_wiersze, srodowisko_kolumny), -100) #środowisko
+nagrody[0, 4] = 100
 przejścia = {}
-przejścia[1] = [1, 2, 3]
+przejścia[1] = [1, 2, 3,4]
 przejścia[2] = [0, 1, 2]
-przejścia[3] = [2, 3]
+przejścia[3] = [2, 3,4]
 przejścia[4] = [1, 2, 3, 4]
 
-for wiersz in range(1, 5):
+for wiersz in range(1, 5): #Podstawienie -1 pod odpowiednie indeksy
     for kolumna in przejścia[wiersz]:
         nagrody[wiersz, kolumna] = -1
 print(nagrody)
@@ -23,7 +23,7 @@ def czy_przeszkoda(curr_wiersz, curr_kolumna):
   else:
     return True
 
-def losowy_start():
+def losowy_start(): #wygenerowanie losowego startu
   wiersz = np.random.randint(srodowisko_wiersze)
   kolumna = np.random.randint(srodowisko_kolumny)
   while czy_przeszkoda(wiersz, kolumna):
@@ -31,13 +31,13 @@ def losowy_start():
     kolumna = np.random.randint(srodowisko_kolumny)
   return wiersz, kolumna
 
-def akcja(wiersz, kolumna, eps):
+def akcja(wiersz, kolumna, eps): #wybór indeksu odpowiedzialnego za kierunek
   if np.random.random() < eps:
     return np.argmax(wartosciQ[wiersz, kolumna])
   else:
     return np.random.randint(4)
 
-def zmiana_pozycji(wiersz, kolumna, akcja):
+def zmiana_pozycji(wiersz, kolumna, akcja): #zmiana indeksów
   nowy_wiersz = wiersz
   nowa_kolumna = kolumna
   if kierunki[akcja] == 'up' and wiersz > 0:
@@ -50,30 +50,32 @@ def zmiana_pozycji(wiersz, kolumna, akcja):
     nowa_kolumna -= 1
   return nowy_wiersz, nowa_kolumna
 
-def droga(wiersz, kolumna):
+def droga(wiersz, kolumna): #wyznaczenie najkrótszej drogi
     curr_wiersz, curr_kolumna = wiersz, kolumna
     najkrótsza_droga = []
     najkrótsza_droga.append([curr_wiersz, curr_kolumna])
     while not czy_przeszkoda(curr_wiersz, curr_kolumna):
-        kierunek = akcja(curr_wiersz, curr_kolumna, 1.)
+        kierunek = akcja(curr_wiersz, curr_kolumna, 1)
         curr_wiersz, curr_kolumna = zmiana_pozycji(curr_wiersz, curr_kolumna, kierunek)
         najkrótsza_droga.append([curr_wiersz, curr_kolumna])
     return najkrótsza_droga
 
-eps = 0.8
+eps = 0.9
 gamma = 0.8
 alfa = 0.8
-for x in range(100):
+for x in range(100): #główna pętla
     wiersz, kolumna = losowy_start()
+    #print(wiersz,kolumna)
     while not czy_przeszkoda(wiersz, kolumna):
         kierunek = akcja(wiersz, kolumna, eps) #kierunek wykonania akcji
         #print(kierunek)
         stary_wiersz, stara_kolumna = wiersz, kolumna #zapis wartości
-        wiersz, kolumna = zmiana_pozycji(wiersz, kolumna, kierunek) #wykonanie akcji
+        wiersz, kolumna = zmiana_pozycji(wiersz, kolumna, kierunek) #wykonanie akcji przemieszczenie
         nagroda = nagrody[wiersz, kolumna] #pobranie nagrody z tablicy
-        stareQ = wartosciQ[stary_wiersz, stara_kolumna, kierunek] #zapis starej wartosci Q, do wzoru
+        stareQ = wartosciQ[stary_wiersz, stara_kolumna, kierunek] #zapis starej wartosci Q, potrzebnej do wzoru
         noweQ = stareQ + (alfa * (nagroda + (gamma * np.max(wartosciQ[wiersz, kolumna])) - stareQ)) #wyliczenie wzóru
         wartosciQ[stary_wiersz, stara_kolumna, kierunek] = noweQ #podpisanie nowej wartości do tablicy
+        #print(wartosciQ)
 print(wartosciQ)
 
 print(droga(4,3))
